573ba025f08fc5bf769123469b191421d486675a
Romario Ferreira <romarioferreira241@gmail.com>
2024-03-29 00:21:22 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+
+
+
+
+display_messages = [
+    "seja feliz",
+    "fique tranquilo",
+    "tudo vai acabar bem"
+]
+
+
+while True:
+    resposta = input("deseja receber um concelho? S/N")
+    if (resposta == "S" or resposta == "s"):
+        mensagem = random.choice(display_messages)
+        print(mensagem)
+        print()
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
